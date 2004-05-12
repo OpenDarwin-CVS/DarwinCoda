ebad8242fad2d94b996c9d81241673a2d7d6e994
bernerus <bernerus>
2004-05-12 22:09:57 +0000

    This file is usually produced by BSD config. In Darwin there is no such process, so we supply vcoda.h explicitly instead.

---
+++ sys/darwin-7.3/coda/vcoda.h
@@ -0,0 +1,11 @@
+/*
+ *  vcoda.h
+ *  darwin_coda
+ *
+ *  Created by Christer Bernerus on Sun May 02 2004.
+ *  Copyright (c) 2004 Christer Bernerus. All rights reserved.
+ *
+ */
+
+#define NVCODA 4
+
