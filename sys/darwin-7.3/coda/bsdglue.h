6b23e3fe95da73fe3de5c2dbc1d0370bd0873146
bernerus <bernerus>
2004-05-12 21:31:36 +0000

    Removed the automagic setting of DARWIN using __APPLE_. Instead, DARWIN is set as a command line option to the compiler. Added some new macros for darwin and sorted the list

--- sys/darwin-7.3/coda/bsdglue.h
+++ sys/darwin-7.3/coda/bsdglue.h
@@ -15,24 +15,39 @@
  */
 
 
-/* remove parse error induced bu __FBSDID. Maybe we should do something sensible with this later on */
-#define __FBSDID(x)
+/* remove parse error induced by __FBSDID. Maybe we should do something sensible with this later on */
 
-#ifdef __APPLE__
-#define DARWIN
-#endif
 
 #ifdef DARWIN
 #include <specdev.h>
 
+#define __FBSDID(x)
 #define v_vflag v_flag
 #define VV_ROOT VROOT
 #define VV_TEXT VTEXT
+
+/* The following macros alter the usage of struct thread to struct proc */
 #define THREAD struct proc
 #define CURTHREAD current_proc();
 #define THREAD2PROC td
+#define uio_td uio_procp
+#define cn_thread cn_proc
+#define td_ucred p_ucred
+#define a_td a_p
+
+
+#define devsw(x) (&cdevsw[major(dev)])
+#define VOP_OPEN(x,y,z,w,t) VOP_OPEN(x,y,z,w)
+#define VOP_BMAP(x,y,z,w,t,r) VOP_BMAP(x,y,z,w,t)
+#define NDFREE(x,y)
+#define lockstatus(x,y) lockstatus(x)
+#define VFS_VGET(x,y,z,w) VFS_VGET(x,y,w)
+#define getnewvnode(x,y,z,w) getnewvnode(VT_OTHER,y,z,w);
+
 #else
+
 #define THREAD struct thread
 #define CURTHREAD curthread
 #define THREAD2PROC td->td_proc
-#endif /* DARWIN */
+
+#endif /* !DARWIN */
