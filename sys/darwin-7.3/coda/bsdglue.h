5f17cb4d716c2bc605717725fc5ba0fa4f29ff3a
bernerus <bernerus>
2004-05-12 21:35:26 +0000

    Added predeclarations for vrefcnt, delay and memcmp. Added VFS lock debug macros. These are not the same as those in the BSD version. Removed the macros that redefined VOP_OPEN, VOP_BMAP and VFS_VGET. They caused the compiler to complain too much. The corresponding changes have been made directly in the code. Added macros to interface to:         selwakeuppri()         vfs_object_create()         VFS_SET         udev2dev()         dev2udev()         VNODEOP_SET()         vnode_pager_setsize()         PROC_LOCK()         PROC_UNLOCK()         udev_t Also NOTBF31 is defined but should go away.

--- sys/darwin-7.3/coda/bsdglue.h
+++ sys/darwin-7.3/coda/bsdglue.h
@@ -20,6 +20,24 @@
 
 #ifdef DARWIN
 #include <specdev.h>
+int         vrefcnt(struct vnode *vp);
+extern void delay();
+extern int  memcmp(const void *, const void *, size_t);
+
+
+
+#ifdef DEBUG_VFS_LOCKS
+
+#define	ASSERT_VOP_LOCKED(vp, s)	assert_vop_locked((vp), __FILE__, __LINE__)
+#define	ASSERT_VOP_UNLOCKED(vp, s)	assert_vop_unlocked((vp), __FILE__, __LINE__)
+
+#else /* !DEBUG_VFS_LOCKS */
+
+#define	ASSERT_VOP_LOCKED(vp, s)
+#define	ASSERT_VOP_UNLOCKED(vp, s)
+
+#endif /* DEBUG_VFS_LOCKS */
+
 
 #define __FBSDID(x)
 #define v_vflag v_flag
@@ -35,14 +53,37 @@
 #define td_ucred p_ucred
 #define a_td a_p
 
-
 #define devsw(x) (&cdevsw[major(dev)])
-#define VOP_OPEN(x,y,z,w,t) VOP_OPEN(x,y,z,w)
-#define VOP_BMAP(x,y,z,w,t,r) VOP_BMAP(x,y,z,w,t)
+//#define VOP_OPEN(x,y,z,w,t) VOP_OPEN(x,y,z,w) - Changes are in the code instead. old redef kept for reference
+//#define VOP_BMAP(x,y,z,w,t,r) VOP_BMAP(x,y,z,w,t)  - Changes are in the code instead. old redef kept for reference
 #define NDFREE(x,y)
 #define lockstatus(x,y) lockstatus(x)
-#define VFS_VGET(x,y,z,w) VFS_VGET(x,y,w)
+#define lockdestroy(x)
+// #define VFS_VGET(x,y,z,w) VFS_VGET(x,y,w) - Changes are in the code instead. old redef kept for reference
 #define getnewvnode(x,y,z,w) getnewvnode(VT_OTHER,y,z,w);
+#define selwakeuppri(p,x) selwakeup(p)
+#define vfs_object_create(vp, p, cred) (0)
+#define VFS_SET(x,y,z) void _no_coda_vfs_set(){}
+
+#define udev2dev(x,y) (x)
+//#define cdev_t dev_t
+#define dev2udev(x) (x)
+#define VNODEOP_SET(x)
+#define vnode_pager_setsize(x,y) myprintf(("would have called vnode_pager_setsize(%p, %d) at %s in %s\n",x,y,__LINE__,__FILE__));
+/* Defining NOTFB31 activates old insqe qnd remque definitions in sys/queue.h */
+#define NOTFB31 (1)
+/* PROC_LOCK seem to be unused in Darwin, there are no signs in the Darwin code that the proc block is
+locked at a number of places almost identical with FreeBSD  I'm leaving the calls in the code
+and define PROC_LOCK and PROC_UNLOCK to be null for the time being */
+#define PROC_LOCK(x)
+#define PROC_UNLOCK(x)
+#define udev_t dev_t
+
+
+
+
+
+
 
 #else
 
