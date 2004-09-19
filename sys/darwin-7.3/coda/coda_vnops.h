5b6e75ed5d073c8d025dbb996a7cc22a75552584
bernerus <bernerus>
2004-09-19 06:48:31 +0000

    Added definition for coda_advlock.

--- sys/darwin-7.3/coda/coda_vnops.h
+++ sys/darwin-7.3/coda/coda_vnops.h
@@ -80,6 +80,7 @@ int coda_vop_nop(void *);
 int coda_fbsd_getpages	(void *);
 int coda_pathconf(void *);
 int coda_pagein(void *);
+int coda_advlock(void *);
 
 int coda_rdwr(struct vnode *vp, struct uio *uiop, enum uio_rw rw,
     int ioflag, struct ucred *cred, THREAD *td);
